bc952231e8771b1b1901bb4aeadf63cfabd3527b
bangld <kobato2096@gmail.com>
2023-03-03 16:04:32 +0700

    Pub/Sub handler

--- server.js
+++ server.js
@@ -1,49 +1,43 @@
-const express = require('express');
-const expressWs = require('express-ws');
-// const redis = require('redis');
-// const publisher = redis.createClient();
-
+const express = require("express");
+const expressWs = require("express-ws");
 const app = express();
 expressWs(app);
+const redis = require("redis");
+const publisher = redis.createClient({
+  url: "redis://redis-user:redis-password@redis:6379",
+});
+const subscriber = client.duplicate();
+await publisher.connect();
+await subscriber.connect();
 
-app.get('/chat', (req, res, next) => {
+await subscriber.subscribe("messages", (message) => {
+  console.log(message);
+  const awss = expressWs.getWss("/chat");
+  awss.clients.forEach((client) => {
+    console.log(client);
+    client.send(message);
+  });
+});
+
+app.get("/chat", (req, res, next) => {
   console.log(req.query);
   res.end();
 });
 
-// ws
-
-app.ws('/chat', function(ws, req) {
-  ws.on('message', function(msg) {
-    console.log('msg');
+app.ws("/chat", function (ws, req) {
+  ws.on("message", async function (msg) {
+    console.log("msg");
+    const message = {
+      id: "123456",
+      message: msg,
+      user_id: 1,
+      timestamp: new Date(),
+    };
+    await publisher.publish("messages", JSON.stringify(message));
   });
-  console.log('socket', req.testing);
+  console.log("socket", req.testing);
 });
-
 
 app.listen(3000, () => {
   console.log(`App running on port 3000...`);
 });
-
-
-// (async () => {
-
-//   const message = {
-//     id: '123456',
-//     message: 'Using Redis Pub/Sub with Node.js',
-//     user_id: 1,
-//     created_at: new Date(),
-//   };
-
-//   await publisher.connect();
-
-//   await publisher.publish('messages', JSON.stringify(message));
-
-//   const subscriber = client.duplicate();
-
-//   await subscriber.connect();
-
-//   await subscriber.subscribe("article", (message) => {
-//     console.log(message); // 'message'
-//   });
-// })();
