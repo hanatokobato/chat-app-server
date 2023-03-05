5929198fd4fa333962c304dde5779e64d4cb4223
bangld <kobato2096@gmail.com>
2023-03-06 06:59:58 +0700

    Fix redis connect

--- server.js
+++ server.js
@@ -1,41 +1,39 @@
-const express = require("express");
-const expressWs = require("express-ws");
+const express = require('express');
+const expressWs = require('express-ws');
 const app = express();
-expressWs(app);
-const redis = require("redis");
+const wsInstance = expressWs(app);
+const redis = require('redis');
 const publisher = redis.createClient({
-  url: "redis://redis:6379",
+  url: 'redis://redis:6379',
 });
 const subscriber = publisher.duplicate();
+publisher.connect();
+subscriber.connect();
 
-app.get("/chat", (req, res, next) => {
+app.get('/chat', (req, res, next) => {
   console.log(req.query);
   res.end();
 });
 
-app.ws("/chat", async function (ws, req) {
-  await publisher.connect();
-  await subscriber.connect();
-  await subscriber.subscribe("messages", (message) => {
+app.ws('/chat', async function (ws, req) {
+  await subscriber.subscribe('messages', (message) => {
     console.log(message);
-    const awss = expressWs.getWss("/chat");
+    const awss = wsInstance.getWss('/chat');
     awss.clients.forEach((client) => {
-      console.log(client);
       client.send(message);
     });
   });
 
-  ws.on("message", async function (msg) {
-    console.log("msg");
+  ws.on('message', async function (msg) {
+    console.log('msg');
     const message = {
-      id: "123456",
+      id: '123456',
       message: msg,
       user_id: 1,
       timestamp: new Date(),
     };
-    await publisher.publish("messages", JSON.stringify(message));
+    await publisher.publish('messages', JSON.stringify(message));
   });
-  console.log("socket", req.testing);
 });
 
 app.listen(3000, () => {
