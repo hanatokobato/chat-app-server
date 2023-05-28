dc46104e44a5c3e95fc713e549ac59eac07bb905
bangld <kobato2096@gmail.com>
2023-05-28 11:19:22 +0700

    Realtime reaction

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -15,6 +15,7 @@ module.exports = (err, req, res, next) => {
   };
 
   if (process.env.NODE_ENV === 'development') {
+    console.log(err.stack);
     jsonResponse = { ...jsonResponse, error: err, stack: err.stack };
   }
 
